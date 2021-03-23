ba9464d8ee06f800361032a9a9c36795d51efdab
AnnaVlad <dav-@mail.ru>
2021-03-23 11:03:59 +0400

    create: modal window

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+$(document).ready(function(){
+
 const hotelSlider = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
@@ -23,9 +25,31 @@ const reviewsSlider = new Swiper('.reviews-slider', {
 
 });
 
-var menuButton = document.querySelector(".menu-button");
-menuButton.addEventListener("click", function () {
-  document
-    .querySelector(".navbar-menu")
-    .classList.toggle("navbar-menu--visible");
-})
+var menuButton = $(".menu-button");
+menuButton.on("click", function () {
+  $(".navbar-menu").toggleClass("navbar-menu--visible");
+});
+
+var modalButton = $('[data-toggle=modal]');
+var closeModalButton = $('.modal__close');
+
+modalButton.on('click', openModal);
+closeModalButton.on('click', closeModal);
+
+  
+  function openModal() {
+    var modalOverlay = $('.modal__overlay');
+    var modalDialog = $('.modal__dialog');
+    modalOverlay.addClass('modal__overlay--visible');
+    modalDialog.addClass('modal__dialog--visible');
+  }
+
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $('.modal__overlay');
+    var modalDialog = $('.modal__dialog');
+    modalOverlay.removeClass('modal__overlay--visible');
+    modalDialog.removeClass('modal__dialog--visible');
+  }
+
+});
